fdc9b36f4d681c47b10d2dd7fd639dccdbc37a32
Anika Nawar <nawarani47@gmail.com>
2024-11-30 05:02:52 -0500

    added tail and assertequal for arrays inside tail file

--- head.js
+++ head.js
@@ -16,3 +16,9 @@ assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([5]), 5);
 assertEqual(head([]), undefined);
 
+
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
