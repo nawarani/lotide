14b2a8b00a8f6e1af43b70fefa5a18fff67ed032
Anika Nawar <nawarani47@gmail.com>
2024-11-30 23:12:19 -0500

    changed code to push method instead of splice

--- without.js
+++ without.js
@@ -23,13 +23,24 @@ const assertArraysEqual = function(actual, expected) {
 // without([1, 2, 3], [1]); // => [2, 3]
 // without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
 
+// const without = (arr, arrtoRemove) => {
+//   let arrWithout = arr.slice();
+//   let delInd = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arrtoRemove.includes(arr[i])) {
+//       arrWithout.splice((i - delInd), 1);
+//       delInd += 1;
+//     }
+//   }
+//   return arrWithout;
+// };
+
+// push method, this is less mind bending
 const without = (arr, arrtoRemove) => {
-  let arrWithout = arr.slice();
-  let delInd = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arrtoRemove.includes(arr[i])) {
-      arrWithout.splice((i - delInd), 1);
-      delInd += 1;
+  let arrWithout = [];
+  for (let elem of arr) {
+    if (!(arrtoRemove.includes(elem))) {
+      arrWithout.push(elem);
     }
   }
   return arrWithout;
