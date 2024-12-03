f5257df1e4aec519ea8c81e80226a5c57573c83d
Anika Nawar <nawarani47@gmail.com>
2024-12-03 09:27:52 -0500

    Added countOnly and test case

--- head.js
+++ head.js
@@ -20,5 +20,5 @@ assertEqual(head([]), undefined);
 
 // Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
+head(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
