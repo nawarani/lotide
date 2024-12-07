f214bea8e8576d6976fa2ae272a1213384b82f8a
Anika Nawar <nawarani47@gmail.com>
2024-12-07 18:37:27 -0500

    map and tests

---
+++ map.js
@@ -0,0 +1,42 @@
+const eqArrays = (actual, expected) => {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+  for (let i in actual) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const map = (arr, callback) => {
+  let newArr = [];
+  for (let element of arr) {
+    newArr.push(callback(element));
+  }
+  return newArr;
+};
+
+// test cases
+// good case
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, (word) => word[0]);
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
+
+// empty
+assertArraysEqual(map([], (x) => x[0]), []);
+
+// no change func
+assertArraysEqual(map(words, (x) => x), words);
+
+
+
