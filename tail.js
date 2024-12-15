2e38e1ea631405042ddbad6ce431ff034c8fcc3d
Anika Nawar <nawarani47@gmail.com>
2024-12-14 20:40:32 -0500

    tail refactor using exports and added tail tests

--- tail.js
+++ tail.js
@@ -1,27 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const assertEqualArray = (actual, expected) => {
-  assertEqual(actual.length, expected.length);
-  for (let i in actual) {
-    assertEqual(actual[i], expected[i]);
-  }
-};
-
 const tail = (arr) => {
   return arr.slice(1);
 };
 
-// test cases
-console.log('original array length test');
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
-
-const result = tail(["Hello", "Lighthouse", "Labs"]); // should return ["Lighthouse", "Labs"]
-assertEqualArray(result, ["Lighthouse", "Labs"]);
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,19 @@
+const assertEqual = require("../assertEqual");
+const tail = require("../tail");
+
+const assertEqualArray = (actual, expected) => {
+  assertEqual(actual.length, expected.length);
+  for (let i in actual) {
+    assertEqual(actual[i], expected[i]);
+  }
+};
+
+// test cases
+console.log('original array length test');
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3);
+
+console.log('New example');
+const result = tail(["Hello", "Lighthouse", "Labs"]); // should return ["Lighthouse", "Labs"]
+assertEqualArray(result, ["Lighthouse", "Labs"]);
