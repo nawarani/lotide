63c4c8c03b57486ab6ffdecd9926fbda0a9d1704
Anika Nawar <nawarani47@gmail.com>
2024-12-14 21:39:51 -0500

    converted head tests for mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,14 +1,28 @@
+const assert = require('chai').assert;
 const head = require("../head");
-const assertEqual = require("../assertEqual");
-
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]), undefined);
 
 
+describe("#head", () => {
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-head(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it("returns 5 for ['5']", () => {
+    assert.strictEqual(head([5]), 5);
+  });
+
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
+  it("does not alter the original function", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    head(words);
+    assert.strictEqual(words.length, 3);
+  });
+
+});
