3ef3b7b769dbda1deb6cd8d2f181959150c5ee04
Anika Nawar <nawarani47@gmail.com>
2024-12-14 20:48:56 -0500

    added tail tests for one and 0 element cases

--- test/tailTest.js
+++ test/tailTest.js
@@ -7,10 +7,28 @@ const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3);
 
-console.log('New example');
+console.log('Happy path');
 const result = tail(["Hello", "Lighthouse", "Labs"]); // should return ["Lighthouse", "Labs"]
 actual = result;
 expected = ["Lighthouse", "Labs"];
+assertEqual(actual.length, expected.length);
+for (let i in actual) {
+  assertEqual(actual[i], expected[i]);
+};
+
+console.log('Empty');
+const result2 = tail([]);
+actual = result2;
+expected = [];
+assertEqual(actual.length, expected.length);
+for (let i in actual) {
+  assertEqual(actual[i], expected[i]);
+};
+
+console.log('One element');
+const result3 = tail(['a']);
+actual = result3;
+expected = [];
 assertEqual(actual.length, expected.length);
 for (let i in actual) {
   assertEqual(actual[i], expected[i]);
